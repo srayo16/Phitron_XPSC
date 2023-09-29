aebe758ef3ec28288d5397b06c3317b33f9a04da
srayo16 <srayosikder@gmail.com>
2023-09-30 01:33:39 +0600

    2

--- Week_01/problem_1.cpp
+++ Week_01/problem_1.cpp
@@ -1,8 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr, int N)
+    {
+        int i = 0, j = 0;
+        long long mx = LONG_MIN;
+        long long s = 0;
+        while (j < N)
+        {
+            s += Arr[j];
+            if (j >= K - 1)
+            {
+                mx = max(mx, s);
+                s -= Arr[i];
+                i++;
+            }
+            j++;
+        }
+
+        return mx;
+    }
+};
+
 int main()
 {
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        ;
+        vector<int> Arr;
+        for (int i = 0; i < N; ++i)
+        {
+            int x;
+            cin >> x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K, Arr, N) << endl;
+    }
     return 0;
 }
---
+++ Week_01/problem_3.cpp
@@ -0,0 +1,41 @@
+// Input:
+// txt = forxxorfxdofr
+// pat = for
+// Output: 3
+// Explanation: for, orf and ofr appears
+// in the txt, hence answer is 3.
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    int search(string pat, string txt)
+    {
+
+        int i = 0, j = 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string pat, txt;
+        cin >> txt >> pat;
+        Solution ob;
+        auto ans = ob.search(pat, txt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
