62d3564aab874d919298def733253eaacec2aeaa
srayo16 <srayosikder@gmail.com>
2023-10-31 23:54:36 +0600

    PS

---
+++ Week_03/PS.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        map<char, int> frq;
+        ll ans = 0;
+        for (auto c : s)
+        {
+            frq[c]++;
+        }
+
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            ll mn = min(frq[i], frq[i - 32]);
+            ans += mn;
+            frq[i] -= mn;
+            frq[i - 32] -= mn;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            int add = max(frq[c], frq[c - 32]);
+            int mn = min(k, add / 2);
+            ans += mn;
+            k -= mn;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
